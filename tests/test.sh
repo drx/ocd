acbc6c4b29950d948610dbf26391529f6887565d
Łukasz Zapart <drx@drx.pl>
2010-04-25 20:57:08 +0200

    Added test script and a test.

---
+++ tests/test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+if [ $# -lt 1 ]
+then
+    echo "Usage: $0 <file>"
+    exit
+fi
+objdump -x $1 | awk '/^ [0-9]* .text/{textvirt=strtonum("0x" $4); textstart=strtonum("0x" $6); textlen=$3} /main$/{mainvirt=strtonum("0x" $1); main=textstart+mainvirt-textvirt; printf "%x %s\n", main, $(NF-1)}' | xargs ../test.py $1
