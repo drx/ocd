1bfdd811e6124ed04bc22684f5b348026b7397b1
Łukasz Zapart <drx@drx.pl>
2010-04-25 21:59:00 +0200

    Fixed test.sh a bit and switched to proper CC flags.

--- tests/test.sh
+++ tests/test.sh
@@ -4,4 +4,4 @@ then
     echo "Usage: $0 <file>"
     exit
 fi
-objdump -x $1 | awk '/^ [0-9]* .text/{textvirt=strtonum("0x" $4); textstart=strtonum("0x" $6); textlen=$3} /main$/{mainvirt=strtonum("0x" $1); main=textstart+mainvirt-textvirt; printf "%x %s\n", main, $(NF-1)}' | xargs ../test.py $1
+objdump -x $1 | awk '/^ [0-9]* .text/{textvirt=strtonum("0x" $4); textstart=strtonum("0x" $6); textlen=$3} /main$/{mainvirt=strtonum("0x" $1); main=textstart+mainvirt-textvirt; printf "%x %s\n", main, $(NF-1)}' | xargs ../ocd.py $1
