dfa06f1905afd558b5832b1683a9ba66e365cd7f
Łukasz Zapart <drx@drx.pl>
2010-06-03 16:38:23 +0200

    Preliminary cleanup.

--- disassemble.py
+++ disassemble.py
@@ -1,21 +1,18 @@
-#!/usr/bin/env python
-
 from libdisassemble.disassemble import *
 
-def disassemble(buf, virt):
+def disassemble_x64(buf, virt):
     FORMAT="INTEL"
     off = 0
 
     result = []
     while off != len(buf):
-#        try:
-            p = Opcode(buf[off:], mode=64)
-            pre = p.getPrefix()
-            length = p.getSize()
-            ins, r, w = p.getOpcode(FORMAT)
-            result.append({'prefix': pre, 'ins': ins, 'loc': virt+off, 'length': length, 'bin': buf[off:off+length], 'r': r, 'w': w})
-            off += length
-# catch-all except... this was bad.
-#        except:
-#            break
+        p = Opcode(buf[off:], mode=64)
+        pre = p.getPrefix()
+        length = p.getSize()
+        ins, r, w = p.getOpcode(FORMAT)
+        result.append({'prefix': pre, 'ins': ins, 'loc': virt+off, 'length': length, 'bin': buf[off:off+length], 'r': r, 'w': w})
+        off += length
     return result
+
+def disassemble(buf, virt, arch='x64'):
+    return disassemble_x64(buf, virt)
