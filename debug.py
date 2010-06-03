f332230472edab55cc26ff41e03a0495f0670604
Łukasz Zapart <drx@drx.pl>
2010-06-03 17:40:53 +0200

    Changed the way debug works.

--- debug.py
+++ debug.py
@@ -6,22 +6,21 @@ Acceptable debug levels: all, graph, misc
 
 debug_options = ['all', 'graph', 'misc']
 
-_debug = []
+__debug = []
 
-def debug_check(cond):
-    global _debug
-    return (cond in _debug or 'all' in _debug)    
+def check(cond):
+    global __debug
+    return (cond in __debug or 'all' in __debug)    
 
-def debug_set(cond=None):
-    global _debug
+def set(cond=None):
+    global __debug
     if cond is None:
-        _debug.append('all')
+        __debug.append('all')
     else:
-        _debug.append(cond)
+        __debug.append(cond)
 
-def debug_sprint(str, cond):
+def sprint(str, cond):
     if debug_check(cond):
         return str
     else:
         return ''
-
